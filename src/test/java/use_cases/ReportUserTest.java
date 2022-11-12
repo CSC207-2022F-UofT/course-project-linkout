d8ff43df63162fc29286b9f0460870ed225dd00a
ryancys1234 <55104016+ryancys1234@users.noreply.github.com>
2022-11-12 12:06:12 -0500

    This commit introduces the testing module and adds the addReport functionality to User.

---
+++ src/test/java/use_cases/ReportUserTest.java
@@ -0,0 +1,15 @@
+package use_cases;
+
+import org.junit.Test;
+import entities.*;
+import record_review_use_case.*;
+
+public class ReportUserTest {
+    @Test
+    public void TestCreatingReport() {
+        //User Tests here after completing the report user use case
+    }
+
+    public void TestAddingReport() {}
+    public void TestAddReportSuccessMessage() {}
+}
