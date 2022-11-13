722911d584ee398ae7cae3dc8e7ecb20ca56e7b3
czz0120 <113061214+czz0120@users.noreply.github.com>
2022-11-12 23:16:59 -0500

    Update RegUserFactory.java

--- src/main/java/entities/RegUserFactory.java
+++ src/main/java/entities/RegUserFactory.java
@@ -1,7 +1,7 @@
 package entities;
 
 public class RegUserFactory {
-    public User creat(String password, String accountName, Profile profile){
+    public User create(String password, String accountName, Profile profile){
         return new RegularUser(password, accountName, profile);
     }
 }
