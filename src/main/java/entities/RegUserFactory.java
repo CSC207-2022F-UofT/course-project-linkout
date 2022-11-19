79fca58a1a63ce5886743258c32e5d47e51f2242
Weilin Yin <alex.yin@mail.utoronto.ca>
2022-11-18 21:37:08 -0500

    update upgrade() function in RegularUser.java

--- src/main/java/entities/RegUserFactory.java
+++
@@ -1,7 +0,0 @@
-package entities;
-
-public class RegUserFactory {
-    public User create(String password, String accountName, Profile profile){
-        return new RegularUser(password, accountName, profile);
-    }
-}
