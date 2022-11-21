f93e3a260604751bd12b67a4894f5f657bc69ebe
cuizezhou <zezhou.cui@mail.utoronto.ca>
2022-11-21 18:51:56 -0500

    Change "Like" method to store account name.

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -8,12 +8,12 @@ import java.util.List;
 
 public abstract class User extends Account{
 
-    private final Profile profile;
-    private final List<User> blocked = new ArrayList<>();
+    private Profile profile;
+    private List<User> blocked = new ArrayList<>();
     private boolean isVIP = false;
-    private final List<User> liked = new ArrayList<>();
-    private final List<User> likedme = new ArrayList<>();
-    private final Hashtable<Integer, List<Object>> reviews = new Hashtable<>();
+    private List<String> liked = new ArrayList<>();
+    private List<User> likedme = new ArrayList<>();
+    private Hashtable<Integer, List<Object>> reviews = new Hashtable<>();
     private float restrictedTime = 0;
 
 
@@ -43,9 +43,7 @@ public abstract class User extends Account{
 
     public Profile displayProfile(){ return this.profile;}
 
-    public List<User> showBlocked(){ return this.blocked;}
-
-    public List<User> showLiked(){ return this.liked;}
+    public List<String> showLiked(){ return this.liked;}
 
     public boolean showVip(){ return this.isVIP;}
 
@@ -81,8 +79,8 @@ public abstract class User extends Account{
         return revBody;
     }
 
-    public void like(User user){
-        this.liked.add(user);
+    public void like(String accName){
+        this.liked.add(accName);
     }
 
 
