d749039bc5f4eff9a0a752bfd36351f5b1ac129c
clara <qingyi.hu@mail.utoronto.ca>
2022-11-22 12:28:41 -0500

    small modifications of review

--- src/main/java/entities/Review.java
+++ src/main/java/entities/Review.java
@@ -3,9 +3,9 @@ package entities;
 public class Review{
     private int rating;
     private String comment;
-    private String username;
+    private String writer;
 
-    private String userviewed;
+    private String receiver;
     private static int count;
     private int id;
 
@@ -16,22 +16,23 @@ public class Review{
      *
      * @param rating the rating of this single Review (1,2,3,4,5).
      * @param comment the comment of this single Review.
-     * @param username the user who receives this single Review.
+     * @param writer the accountname of the user who wrote this single Review.
+     * @param receiver the accountname of the user who receives this single Review.
      */
 
-    public Review(int rating, String comment, String username, String userviewed) {
+    public Review(int rating, String comment, String writer, String receiver) {
         this.rating = rating;
         this.comment = comment;
-        this.userviewed = userviewed;
-        this.username = username;
+        this.writer = writer;
+        this.receiver = receiver;
         this.id = count++;
     }
 
-    public Review(int rating, String comment, String username, String userviewed, int id) {
+    public Review(int rating, String comment, String writer, String receiver, int id) {
         this.rating = rating;
         this.comment = comment;
-        this.userviewed = userviewed;
-        this.username = username;
+        this.writer = writer;
+        this.receiver = receiver;
         this.id = id;
     }
 
@@ -59,16 +60,16 @@ public class Review{
         this.comment = comment;
     }
 
-    public User getUser() {
-        return db.findUser(username);
+    public User getWriter() {
+        return db.findUser(writer);
     }
 
-    public User getUserCommented() {
-        return db.findUser(userviewed);
+    public User getReceiver() {
+        return db.findUser(receiver);
     }
 
-    public void setUser(String username) {
-        this.username = username;
+    public void setWriter(String writer) {
+        this.writer = writer;
     }
 
 }
