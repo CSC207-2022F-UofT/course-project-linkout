102116494110e2676b09a18278be300a089d9fc7
cuizezhou <zezhou.cui@mail.utoronto.ca>
2022-11-30 13:46:54 -0500

    Create Testing class for UserLike use case.

---
+++ src/test/java/useCases/user_action_use_case/UserActInteractorTest.java
@@ -0,0 +1,13 @@
+package useCases.user_action_use_case;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserActInteractorTest {
+
+
+    @Test
+    void like() {
+    }
+}
