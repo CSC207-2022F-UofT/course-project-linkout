37802d2629d6677dafd14bfa7b1742063d0ea8ab
Ryan Shi <55104016+ryancys1234@users.noreply.github.com>
2022-12-06 23:44:06 -0500

    Update RecordReportInteractor.java

--- src/main/java/use_cases/record_report_use_case/RecordReportInteractor.java
+++ src/main/java/use_cases/record_report_use_case/RecordReportInteractor.java
@@ -6,13 +6,29 @@ public class RecordReportInteractor implements RecordReportInputBoundary {
     RecordReportGateway recordReportGateway;
     boolean success;
     String adminID;
-
+    
+    /**
+     * The constructor of an interactor object.
+     * @param recordReportOB        The instance of the output boundary.
+     * @param recordReportGateway   The instance of the gateway.
+     * @param adminID               The admin ID (to be determined outside this use case).
+     */
     public RecordReportInteractor(RecordReportOutputBoundary recordReportOB, RecordReportGateway recordReportGateway, String adminID) {
         this.recordReportOB = recordReportOB;
         this.recordReportGateway = recordReportGateway;
         this.adminID = adminID;
     }
-
+    
+    /**
+     * The primary function of the functionality of the record report use case.
+     * A report object is created and stored in the user's reports attribute.
+     * The admin is simultaneously notified. If this succeeds, the success view
+     * is prepared. If this fails, the catch clause will be reached and the fail
+     * view is prepared.
+     *
+     * @param r The instance of the input data.
+     * @return  The responseView function.
+     */
     @Override
     public RecordReportOutputData createReport(RecordReportInputData r) {
         Report report = new Report(r.getReportingUserID(), r.getReportedUserID(),
