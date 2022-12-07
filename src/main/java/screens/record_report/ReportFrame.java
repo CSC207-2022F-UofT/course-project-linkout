e55c55917117f02ca8ace659294974282d1bd3b0
xumiche7 <98936885+xumiche7@users.noreply.github.com>
2022-12-07 17:45:30 -0500

    Updated record_report

--- src/main/java/screens/record_report/ReportFrame.java
+++ src/main/java/screens/record_report/ReportFrame.java
@@ -1,9 +1,15 @@
 package screens.record_report;
 
-import use_cases.record_report_use_case.RecordReportController;
+import screens.review_screen.IReviewViewImplementation;
+import screens.review_screen.LabelTextPanel;
+import screens.review_screen.ReviewCreationScreen;
+import use_cases.record_report_use_case.*;
+import use_cases.regular_user_register_use_case.UserGateway;
+import use_cases.review_use_case.*;
 
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -15,14 +21,30 @@ public class ReportFrame extends JFrame implements ActionListener {
     JTextField reportText;
     JTextField additional;
     JComboBox<String> category;
+
+    public static void main(String[] args) throws IOException {
+        RecordReportOutputData recordReportOD = new RecordReportOutputData();
+        RecordReportResultFrame viewReport = new RecordReportResultFrame();
+        UserGateway recordReportGateway = new UserGateway(System.getProperty("user.dir"));
+        RecordReportPresenter reportPresenter = new RecordReportPresenter(recordReportOD, viewReport);
+        ReportDatabaseGateway recordReportDatabaseGateway = new ReportDatabase(System.getProperty("user.dir"));
+        RecordReportInteractor reportInteractor = new RecordReportInteractor(reportPresenter, recordReportGateway,
+                recordReportDatabaseGateway, "Admin");
+        RecordReportController recordReportController = new RecordReportController(reportInteractor);
+        ReportFrame frame = new ReportFrame(recordReportController, "Admin");
+        frame.setVisible(true);
+    }
     public ReportFrame(RecordReportController controller, String userID) {
+
+        setBounds(150, 150, 780, 442);
+
         this.controller = controller;
         this.userID = userID;
 
         JLabel title = new JLabel("Report this user");
         title.setAlignmentX(0.5f);
 
-        reportingUserID = new JTextField("Your username");
+        reportingUserID = new JTextField("Your username:");
         reportText = new JTextField("Write your report here");
         additional = new JTextField("Include any additional information here");
         JButton ok = new JButton("OK");
@@ -35,18 +57,22 @@ public class ReportFrame extends JFrame implements ActionListener {
                 "Other"};
         category = new JComboBox<String>(categories);
 
-        ok.addActionListener(this);
-        cancel.addActionListener(this);
 
         JPanel buttons = new JPanel();
         buttons.add(ok);
         buttons.add(cancel);
 
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        ok.addActionListener(this);
+        cancel.addActionListener(this);
+
+        getContentPane().setLayout(
+                new BoxLayout(getContentPane(), BoxLayout.Y_AXIS)
+        );
         this.add(reportingUserID);
         this.add(reportText);
         this.add(additional);
         this.add(category);
+        this.add(buttons);
     }
 
     @Override
