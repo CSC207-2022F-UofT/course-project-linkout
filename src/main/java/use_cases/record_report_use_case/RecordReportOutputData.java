466741fc0e1cbaf0a4e86e2806e92d0a0bccddc0
Ryan Shi <55104016+ryancys1234@users.noreply.github.com>
2022-12-06 23:44:32 -0500

    Update RecordReportOutputData.java

--- src/main/java/use_cases/record_report_use_case/RecordReportOutputData.java
+++ src/main/java/use_cases/record_report_use_case/RecordReportOutputData.java
@@ -2,10 +2,18 @@ package use_cases.record_report_use_case;
 
 public class RecordReportOutputData {
     private String timestamp;
-
+    
+    /**
+     * The constructor instantiating an output data object containing the timestamp
+     * corresponding to the report's creation.
+     */
     public RecordReportOutputData() {
         timestamp = "";
     }
+    
+    /*
+    Getters and setters.
+     */
     public String getTimestamp() { return timestamp; }
     public void setTimestamp(String timestamp) { this.timestamp = timestamp; }
 }
