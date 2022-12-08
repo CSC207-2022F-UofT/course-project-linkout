8132e9ed086fa14cfaad26e46d676c77dfbe5dda
yifeiiZhang <yifeii.zhang@mail.utoronto.ca>
2022-12-08 16:17:38 -0500

    modify usergateway, UserLoginDsGateway delete fileuser

--- src/main/java/use_cases/user_login_use_case/UserLoginDsGateway.java
+++ src/main/java/use_cases/user_login_use_case/UserLoginDsGateway.java
@@ -1,6 +1,9 @@
 package use_cases.user_login_use_case;
 
+import javax.management.InvalidAttributeValueException;
+import java.io.IOException;
+
 public interface UserLoginDsGateway {
-    boolean MatchingNameAndPassword(String accountName, String Password);
-    boolean NotExist(String accountName);
+    boolean MatchingNameAndPassword(String accountName, String Password) throws IOException, InvalidAttributeValueException;
+    boolean NotExist(String accountName) throws IOException, InvalidAttributeValueException;
 }
