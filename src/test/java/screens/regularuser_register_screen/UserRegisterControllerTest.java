da94d386ca33b4748f4df08bc6df9f770d592757
yifeiiZhang <yifeii.zhang@mail.utoronto.ca>
2022-12-08 17:29:03 -0500

    add javadoc

---
+++ src/test/java/screens/regularuser_register_screen/UserRegisterControllerTest.java
@@ -0,0 +1,12 @@
+package screens.regularuser_register_screen;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRegisterControllerTest {
+
+    @Test
+    void create() {
+    }
+}
